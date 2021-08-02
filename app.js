893f94c675bf5fd5eed0755ab2ff7918ed19a71c
Yuan Gao <yuan@SwordFish.local>
2021-08-01 23:01:24 -0500

    add start

--- app.js
+++ app.js
@@ -112,6 +112,7 @@ app.use((err, req, res, next) => {
     res.status(starusCode).render("error", {err});
 });
 
-app.listen(3000, () => {
-    console.log("serving on port 3000")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`serving on port ${port}`)
 });
